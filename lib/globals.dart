7a8c45f265afd2df894d6bc1a2d330a92a8f4bc4
Azimul-Haque <orbachinujbuk@gmail.com>
2022-03-28 22:06:27 +0600

    Commit Mon 03/28/2022 22:06:27.20 17410

--- lib/globals.dart
+++ lib/globals.dart
@@ -64,7 +64,7 @@ appBarStyle() {
   return Ink(
     decoration: BoxDecoration(
       gradient: LinearGradient(
-        colors: [Colors.lightBlue!, Colors.lightBlue],
+        colors: [Colors.lightBlue[600]!, Colors.lightBlue[200]!],
         begin: const FractionalOffset(0.0, 0.0),
         end: const FractionalOffset(1.0, 0.0),
         stops: [0.0, 1.0],
